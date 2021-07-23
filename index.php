85d07d0b8dd87ab037c6bbc8d80406c6c800d265
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-07-23 14:58:35 +0200

    parte 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza -->
+
+<?php
+    $par = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iure sit quam praesentium magni dolore.';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1><?php echo $par ?></h1>
+</body>
+</html>
